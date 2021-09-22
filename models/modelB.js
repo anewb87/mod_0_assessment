4ec17f636cd9d521d4ff36ed0321d6b66a11163a
Alexis Newby <alexis.newby@gmail.com>
2021-09-22 08:57:56 -0600

    Create developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: "Lexy",
+  experienceLevel: "novice",
+  age: 34,
+  typingWordsPerMinute: 70,
+  isExcited: true,
+  isAnxious: true,
+  dataTypesLearned: ["string", "integer", "boolean", "array", "object"],
+  workCompleted: ["Module 0", "Module 0 Homework", "Module 1 Prework"]
+};
