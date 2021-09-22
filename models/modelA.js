df657fcc7d02577e3dbd81d2a898e2fe8659ff91
Alexis Newby <alexis.newby@gmail.com>
2021-09-22 08:50:19 -0600

    Create dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name: "Barney",
+  catchprase: "I love you",
+  age: 53,
+  heightINFEET: 8,
+  isFriendly: true,
+  isTalkative: true,
+  colors: ["purple", "green", "white"],
+  bestFriends: ["Lucy", "Charlie", "Mac"]
+};
