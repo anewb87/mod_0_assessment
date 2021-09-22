b77c75c7dbd653cd9cd44b906d35937c4ce7f10e
Alexis Newby <alexis.newby@gmail.com>
2021-09-22 08:43:22 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,17 @@
+var movie = "Slumdog Millionaire"
+
+var isEntertaining = true
+
+movie = "Wonder Woman"
+
+var num4 = 8
+
+var num6 = 10
+
+var product = num6 * num4
+
+var favoriteFoods = ["chicken tikka masala", "Chicago style pizza", "salad"]
+
+var firstElement = favoriteFoods.shift()
+
+console.log(movie.length)
